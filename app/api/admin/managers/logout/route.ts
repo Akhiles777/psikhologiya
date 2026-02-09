28d1fb9fb4dc52e19df2e878fc8d17441e1deafa
Gasan <gasanalunkachev@gmail.com>
2026-02-09 16:28:19 +0300

    ff

--- app/api/admin/managers/logout/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/admin/logout/route.ts
-import { NextRequest, NextResponse } from 'next/server';
-import { destroySession } from '@/lib/auth';
-
-export async function POST() {
-  await destroySession();
-  return NextResponse.json({ success: true });
-}
